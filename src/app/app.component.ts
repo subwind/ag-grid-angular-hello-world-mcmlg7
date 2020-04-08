5be7245b76ec110deae6013b4178c32c2feabc61
subwind <subwind@gmail.com>
2020-04-08 12:08:29 +0800

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,33 +11,27 @@ import {GridOptions} from "ag-grid/main";
 1.每頁顯示筆數
 2.可切換每頁顯示筆數
 3.上下頁
-4.排序
-5.外部按鈕可以控制grid
-6.全選、單選、複選
+4.頁碼(?)
+5.排序
+6.外部按鈕可以控制grid
+7.全選、單選、複選
  */
 
 export class AppComponent  {
 
     public columnDefs;
     public rowData;
-    private gridOptions:GridOptions
+    private gridOptions:GridOptions;
     constructor() {
     }
 
     ngOnInit(){
       this.gridOptions = <GridOptions>{};
+      //this.gridOptions.rowModelType = 'client-side';
       this.createColumnDefs();
       this.createRowData();
+      //this.reloadData();
       
-    }
-
-    public createColumnDefs(){
-      this.columnDefs = [
-        {headerName: '#',width: 30 ,checkboxSelection: true},
-        {field: 'make',suppressSorting: true },
-        {field: 'model' },
-        {field: 'price'}
-      ]
     }
 
     public createRowData(){
@@ -48,4 +42,36 @@ export class AppComponent  {
       ]
     }
 
+    public reloadData(){
+      this.rowData = [
+        { make: 'T', model: 'C', price: 35 },
+        { make: 'F', model: 'M', price: 32 },
+        { make: 'P', model: 'B', price: 72 }
+      ]
+    }
+
+    public createColumnDefs(){
+      this.columnDefs = [
+        {headerName: '#',width: 30 ,checkboxSelection: true},
+        {field: 'make',sort: 'desc' ,sortable: true },
+        {field: 'model',sortable: true,comparator:this.modelComparator},
+        {field: 'price',sortable: true}
+      ]
+    }
+
+    //客製排序
+    public modelComparator = (valueA, valueB, nodeA, nodeB, isInverted)=>{
+      //console.log(this.rowData);
+      //console.log(valueA, valueB, nodeA, nodeB, isInverted);
+      this.reloadData();
+      return valueA - valueB;
+    }
+
+    public calculateRowCount(){
+      if(this.gridOptions.api && this.rowData){
+        var model = this.gridOptions.api.getModel();
+      }
+    }
+
 }
+
