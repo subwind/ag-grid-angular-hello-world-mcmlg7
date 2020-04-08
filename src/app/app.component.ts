a55958a09c311afa6979746bbe339797bfa31427
subwind <subwind@gmail.com>
2020-04-08 09:21:26 +0800

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import {GridOptions} from "ag-grid/main";
 
 @Component({
   selector: 'my-app',
@@ -17,16 +18,34 @@ import { Component } from '@angular/core';
 
 export class AppComponent  {
 
-    columnDefs = [
-        {field: 'make' },
+    public columnDefs;
+    public rowData;
+    private gridOptions:GridOptions
+    constructor() {
+    }
+
+    ngOnInit(){
+      this.gridOptions = <GridOptions>{};
+      this.createColumnDefs();
+      this.createRowData();
+      
+    }
+
+    public createColumnDefs(){
+      this.columnDefs = [
+        {headerName: '#',width: 30 ,checkboxSelection: true},
+        {field: 'make',suppressSorting: true },
         {field: 'model' },
         {field: 'price'}
-    ];
+      ]
+    }
 
-    rowData = [
+    public createRowData(){
+      this.rowData = [
         { make: 'Toyota', model: 'Celica', price: 35000 },
         { make: 'Ford', model: 'Mondeo', price: 32000 },
         { make: 'Porsche', model: 'Boxter', price: 72000 }
-    ];
+      ]
+    }
 
 }
