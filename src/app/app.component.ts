d3fee19dc7a51362686479fb69dcda5c8115d6fa
subwind <subwind@gmail.com>
2020-04-09 11:51:56 +0800

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,9 +21,16 @@ import {GridOptions} from "ag-grid/main";
 
 export class AppComponent  {
 
-    private gridOptions:GridOptions;
+    public gridOptions:GridOptions;//測試用
+    public gridOptionsSample1:GridOptions;//單選範例-有checkbox
+    public gridOptionsSample2:GridOptions;//多選範例-有checkbox
+    public gridOptionㄋ
     public columnDefs;
+    public columnDefs1;
+    public columnDefs2;
     public rowData;
+    public rowData1;
+    public rowData2;
     public rowCount:string ='';
     public selData =[];
 
@@ -32,14 +39,20 @@ export class AppComponent  {
 
     ngOnInit(){
       this.gridOptions = <GridOptions>{};
+      this.gridOptionsSample1 = <GridOptions>{};
+      this.gridOptionsSample2 = <GridOptions>{};
 
       //this.gridOptions.pagination = true;
       //this.gridOptions.paginationPageSize = 10;
-      this.gridOptions.isRowSelectable = ()=>{
-        return true;
-      }
       this.gridOptions.rowSelection = 'multiple';
       this.gridOptions.rowMultiSelectWithClick = true;
+      //範例1
+      this.gridOptionsSample1.rowSelection = 'single';
+      this.gridOptionsSample1.rowMultiSelectWithClick = true;
+      //範例2
+      this.gridOptionsSample2.rowSelection = 'multiple';
+      this.gridOptionsSample2.rowMultiSelectWithClick = true;
+
       this.createColumnDefs();
       this.createRowData();
       //this.reloadData();
@@ -52,6 +65,19 @@ export class AppComponent  {
         { make: 'Ford', model: 'Mondeo', price: 32000 },
         { make: 'Porsche', model: 'Boxter', price: 72000 }
       ]
+
+      this.rowData1 =[
+        { make: 'Toyota', model: 'Celica', price: 35000 },
+        { make: 'Ford', model: 'Mondeo', price: 32000 },
+        { make: 'Porsche', model: 'Boxter', price: 72000 }
+      ]
+
+      this.rowData2 =[
+        { make: 'Toyota', model: 'Celica', price: 35000 },
+        { make: 'Ford', model: 'Mondeo', price: 32000 },
+        { make: 'Porsche', model: 'Boxter', price: 72000 }
+      ]
+
     }
 
     public reloadData(){
@@ -73,6 +99,21 @@ export class AppComponent  {
         {field: 'model',sortable: true,comparator:this.modelComparator},
         {field: 'price',sortable: true}
       ]
+
+      this.columnDefs1 = [
+        {headerName:'#',width:30,checkboxSelection:true},
+        {field: 'make',sort: 'desc' ,sortable: true },
+        {field: 'model',sortable: true},
+        {field: 'price',sortable: true}
+      ]
+
+      this.columnDefs2 = [
+        {headerName:'#',width:30,checkboxSelection:true},
+        {field: 'make',sort: 'desc' ,sortable: true },
+        {field: 'model',sortable: true},
+        {field: 'price',sortable: true}
+      ]
+
     }
 
     //客製排序
@@ -88,21 +129,49 @@ export class AppComponent  {
         this.calculateRowCount();
     }
 
+    public onReady1(){
+      console.log('onReady1');
+    }
+
+    public onReady2(){
+      console.log('onReady2');
+    }
+
     public onRowSelected(event){
         console.log(event.node);
         console.log(event.node.gridApi.getSelectedRows());
         let selectData = event.node.gridApi.getSelectedRows();
+        let selectNode = event.node.gridApi.getSelectedNodes();
+        console.log(selectNode)
         let selectionCounts  = selectData.length;
-        if (selectionCounts > 2) {
-          let lastIndex = 0;
+        let selectionNodeCounts = selectNode.length;
+        // if (selectionCounts > 2) {
+        //   selectData.forEach((item,index)=>{
+        //     if(item.make==event.node.data.make){
+        //       this.gridOptions.api.deselectIndex(index, true); // This works!
+        //       console.log(index,'lastIndex');
+        //     }
+        //   })
+        //   //this.gridOptions.api.deselectIndex(lastIndex, true); // This works!
+        // }
+        if(selectionCounts>2){
           selectData.forEach((item,index)=>{
-            if(item.make==event.node.data.mark){
-              lastIndex = index;
+            if(item.make==event.node.data.make){
+                var oldestNode = this.gridOptions.api.getSelectedNodes()[index]; // get the first node, to be popped out
+                oldestNode.setSelected(false);
             }
           })
-          this.gridOptions.api.deselectIndex(lastIndex, true); // This works!
         }
+
         //console.log(this.gridOptions)
+    }
+
+    public onRowSelected1(event){
+      console.log(event.node.data,'onRowSelected1');
+    }
+
+    public onRowSelected2(event){
+      console.log(event.node.data,'onRowSelected2');
     }
 
     public calculateRowCount(){
